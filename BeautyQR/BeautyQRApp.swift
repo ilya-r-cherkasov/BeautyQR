d005d74a1d583925803020d5c9fcf729fb868013
Ilya Cherkasov <cherkasov-i@surfstudio.ru>
2021-12-09 21:27:43 +0300

    Initial Commit

---
+++ BeautyQR/BeautyQRApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeautyQRApp.swift
+//  BeautyQR
+//
+//  Created by Ilya Cherkasov on 09.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct BeautyQRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
