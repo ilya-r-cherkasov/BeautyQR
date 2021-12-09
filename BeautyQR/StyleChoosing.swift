4cff1014586b8fe4086ae71e5c874415b47cf572
Ilya Cherkasov <cherkasov-i@surfstudio.ru>
2021-12-10 01:22:36 +0300

    added qr library

--- BeautyQR/StyleChoosing.swift
+++ BeautyQR/StyleChoosing.swift
@@ -6,14 +6,45 @@
 //
 
 import SwiftUI
+import EFQRCode
 
 struct StyleChoosing: View {
     
     @Binding var sourceImage: UIImage
     
     var body: some View {
-        Image(uiImage: sourceImage)
+        Image(uiImage: generate(for: recognize().first ?? ""))
             .resizable()
+            .frame(width: 300, height: 300, alignment: .center)
+    }
+    
+    func recognize() -> [String] {
+        if let testImage = sourceImage.cgImage {
+            let codes = EFQRCode.recognize(testImage)
+            if !codes.isEmpty {
+                print("There are \(codes.count) codes")
+                for (index, code) in codes.enumerated() {
+                    print("The content of QR Code \(index) is \(code).")
+                }
+            } else {
+                print("There is no QR Codes in testImage.")
+            }
+            return codes
+        }
+        return []
+    }
+    
+    func generate(for url: String) -> UIImage {
+        if let image = EFQRCode.generate(
+            for: url,
+            watermark: UIImage(named: "Cat")?.cgImage
+        ) {
+            print("Create QRCode image success \(image)")
+            return UIImage(cgImage: image)
+        } else {
+            print("Create QRCode image failed!")
+            return UIImage()
+        }
     }
     
 }
