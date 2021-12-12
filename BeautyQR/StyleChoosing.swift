b372de6fdd1764a16eed294f77ed2db28a13308d
Ilya Cherkasov <cherkasov-i@surfstudio.ru>
2021-12-12 18:03:57 +0300

    added carousel

--- BeautyQR/StyleChoosing.swift
+++ BeautyQR/StyleChoosing.swift
@@ -12,11 +12,76 @@ import WidgetKit
 struct StyleChoosing: View {
     
     @Binding var sourceImage: UIImage
+    var spacing: CGFloat = 15.0
+    var trailingSpace: CGFloat = 150
+    @GestureState var offset: CGFloat = 0
+    @State var currentIndex: Int = 0
+    @State var imageList = [UIImage]()
     
     var body: some View {
-        Image(uiImage: generate(for: recognize().first ?? ""))
-            .resizable()
-            .frame(width: 300, height: 300, alignment: .center)
+        NavigationView {
+            GeometryReader { proxy in
+                let width = proxy.size.width - (trailingSpace - spacing)
+                let adjustMentWidth = (trailingSpace / 2) - spacing
+                HStack(spacing: spacing){
+                    ForEach(imageList, id: \.self) { image in
+                        GeometryReader { proxy in
+                            let scale = getScale(proxy: proxy)
+                            Image(uiImage: image)
+                                .resizable()
+                                .scaleEffect(scale)
+                        }
+                        .padding()
+                        .frame(
+                            width: max(proxy.size.width - trailingSpace, 0),
+                            height: max(proxy.size.width - trailingSpace, 0)
+                        )
+                    }
+                }
+                .padding(.horizontal, spacing)
+                .offset(x: (CGFloat(currentIndex) * -width) + adjustMentWidth + offset)
+                .gesture(
+                    DragGesture()
+                        .updating($offset, body: { value, out, _ in
+                            out = value.translation.width
+                        })
+                        .onEnded({ value in
+                            let offsetX = value.translation.width
+                            let progress = -offsetX / width
+                            let roundIndex = progress.rounded()
+                            currentIndex = max(min(currentIndex + Int(roundIndex), 5), 0)
+                        })
+                )
+            }
+            .animation(.easeInOut, value: offset == 0)
+            .onAppear {
+                for num in 1...6 {
+                    let image = generate(for: recognize().first ?? "", placeholer: "placeholder\(num)")
+                    imageList.append(image)
+                }
+            }
+        }
+    }
+    
+    private func getScale(proxy: GeometryProxy) -> CGFloat {
+                
+        let mid = UIScreen.main.bounds.width / 2
+        let left = mid * 0.2
+        let right = mid * 1.8
+                
+        var scale: CGFloat = 1
+        
+        let x = proxy.frame(in: .global).midX
+        
+        if (left...mid).contains(x) {
+            scale =  1 + abs(x - left) * 0.002
+        }
+        
+        if (mid...right).contains(x) {
+            scale =  1 + abs(x - right) * 0.002
+        }
+        
+        return scale
     }
     
     func recognize() -> [String] {
@@ -34,11 +99,11 @@ struct StyleChoosing: View {
         }
         return []
     }
-    
-    func generate(for url: String) -> UIImage {
+
+    func generate(for url: String, placeholer: String) -> UIImage {
         if let image = EFQRCode.generate(
             for: url,
-            watermark: UIImage(named: "Cat")?.cgImage
+            watermark: UIImage(named: placeholer)?.cgImage
         ) {
             print("Create QRCode image success \(image)")
             if let pngRepresentation = UIImage(cgImage: image).pngData(),
@@ -54,4 +119,10 @@ struct StyleChoosing: View {
         }
     }
     
+}
+
+struct StyleChoosing_Previews: PreviewProvider {
+    static var previews: some View {
+        MainScreen()
+    }
 }
